8abe0491c426259b41e9cdfe6869d7091c28eae0
Fatih Soysal <fatihsoysal@outlook.com>
2017-11-22 12:19:14 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# jquery.openSound
+
+You can open and play sound this plugin.
+
+## Getting Started
+
+<script src='jquery-2.2.0.min.js'></script>
+<script src='jquery.openSound.js'></script>
+
+### Usage
+
+ //Open Sound
+ $.openSound('http://example.org/sound.wav')
+ $.openSound('/attachments/sounds/1234.wav')
+ $.openSound('/attachments/sounds/1234.mp3')
+ 
+ //Stop Sound
+ $.stopSound();
+
+
+### Demo
+
+https://codepen.io/fatihsoysal/pen/bYvybY
+
+Enjoy it!
+
+That's all folks!
+
+<a href="http://fatihsoysal.com">www.fatihsoysal.com</a>
+
+```
---
+++ jquery.openSound.js
@@ -0,0 +1,25 @@
+/**
+ * @author Fatih Soysal <fatihsoysal@outlook.com>
+ * Copyright (c) 2011 Fatih Soysal - released under MIT License
+ * Usage: $.openSound('http://example.org/sound')
+ * $.openSound('http://example.org/sound.wav')
+ * $.openSound('/attachments/sounds/1234.wav')
+ * $.openSound('/attachments/sounds/1234.mp3')
+ * $.stopSound();
+**/
+
+(function ($) {
+    $.extend({
+        openSound: function () {
+            return $(
+                   '<audio class="sound-player" autoplay="autoplay" style="display:none;">'
+                     + '<source src="' + arguments[0] + '" />'
+                     + '<embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="false"/>'
+                   + '</audio>'
+                 ).appendTo('body');
+        },
+        stopSound: function () {
+            $(".sound-player").remove();
+        }
+    });
+})(jQuery);
